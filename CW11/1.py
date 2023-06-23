0c4ec872c358a70ccb06f798345f31f6978ce62b
MrSinxx <sinaleylaz@yahoo.com>
2023-06-23 16:23:40 +0330

    added def Get

--- CW11/1.py
+++ CW11/1.py
@@ -10,12 +10,18 @@ todo_list = []
 class TodoRequestHandler(BaseHTTPRequestHandler):
     
     
-    def set_headers(self,status_code=200,content_type= "text/plain"):
-    
-    
-    
+    def _set_headers(self,status_code=200,content_type= "text/plain"):
+        self.send_response(status_code)
+        self.send_header("Content-type",content_type)
+        self.end_headers()
+        
     def do_GET(self):
-        ##
+        if self.path == "/":
+            self._set_headers(200,"application/json")
+            self.wfile.write(json.dumps(todo_list).encode())
+        else:
+            self._set_headers(404)
+            self.wfile.write("Not Found".encode())
         
         
     def do_POST(self):
