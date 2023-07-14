739c373874f3d63f97bc754b19e3bd6dbf1353f4
MrSinxx <sinaleylaz@yahoo.com>
2023-07-14 11:35:29 +0330

    Added the complete relations

--- CW_15/BlogPlatform/BlogApp/models.py
+++ CW_15/BlogPlatform/BlogApp/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from category.models import Category
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey("Author", on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     publication_date = models.DateTimeField(auto_now_add=True)
 
@@ -14,18 +22,10 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
-
-
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-    bio = models.TextField()
-
-    def __str__(self):
-        return self.name
