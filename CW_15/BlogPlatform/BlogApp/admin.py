6ee1227bc8c89eae2b100c8f7a87823599c04264
MrSinxx <sinaleylaz@yahoo.com>
2023-07-14 15:24:21 +0330

    added comment to admin.py

--- CW_15/BlogPlatform/BlogApp/admin.py
+++ CW_15/BlogPlatform/BlogApp/admin.py
@@ -1,7 +1,9 @@
+from calendar import c
 from django.contrib import admin
-from .models import Post, Author
+from .models import Post, Author, Comment
 from category.models import Category
 
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Author)
+admin.site.register(Comment)
