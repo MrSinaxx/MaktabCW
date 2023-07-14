0c87e2d3cbffe4964b987a7e7d7b6159da099857
MrSinxx <sinaleylaz@yahoo.com>
2023-07-14 10:42:59 +0330

    Added BlogApp.views.py

--- CW_15/BlogPlatform/BlogApp/views.py
+++ CW_15/BlogPlatform/BlogApp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Author
 
-# Create your views here.
+
+def home(request):
+    posts = Post.objects.all()
+    authors = Author.objects.all()
+    return render(request, "home.html", {"posts": posts, "authors": authors})
+
+
+def all_posts(request):
+    posts = Post.objects.all()
+    return render(request, "all_posts.html", {"posts": posts})
+
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "post_details.html", {"post": post})
+
+
+def author_list(request):
+    authors = Author.objects.all()
+    return render(request, "author_list.html", {"authors": authors})
+
+
+def author_details(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    return render(request, "author_details.html", {"author": author})
