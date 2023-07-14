8d9e9e3c0083c452c0ed8a2d69d92fe29e8f7778
MrSinxx <sinaleylaz@yahoo.com>
2023-07-14 10:38:13 +0330

    Added the BlogApp.models

--- CW_15/BlogPlatform/BlogApp/models.py
+++ CW_15/BlogPlatform/BlogApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
