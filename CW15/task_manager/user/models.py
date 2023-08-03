130aa94ebb6eb9dd5790906f43998f0f71a0a40f
MrSinxx <sinaleylaz@yahoo.com>
2023-08-03 16:01:06 +0330

    feat:class User(AbstractBaseUser)

--- CW15/task_manager/user/models.py
+++ CW15/task_manager/user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from manger import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    photo = models.ImageField(upload_to="user_photos/", blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
+        return self.username
