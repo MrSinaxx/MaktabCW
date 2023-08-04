c292f6cf1e660c4ea4c98695c68eb5c229a2a434
MrSinxx <sinaleylaz@yahoo.com>
2023-08-04 10:58:24 +0330

    feat: views for register user and login user

--- CW15/task_manager/user/views.py
+++ CW15/task_manager/user/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
-# Create your views here.
+
+def register_user(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
