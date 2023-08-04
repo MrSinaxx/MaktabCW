c9948237d921a92c9a5c7e1583fb52de389eb8bf
MrSinxx <sinaleylaz@yahoo.com>
2023-08-04 10:54:26 +0330

    Fix:manger and model to handle the superuser

--- CW15/task_manager/user/manger.py
+++ CW15/task_manager/user/manger.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 
 class UserManager(BaseUserManager):
@@ -14,7 +18,9 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
