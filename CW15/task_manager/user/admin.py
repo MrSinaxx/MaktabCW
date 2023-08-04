d68bf56b005a712e9da50cf05b53eab3fce18ca3
MrSinxx <sinaleylaz@yahoo.com>
2023-08-04 10:55:21 +0330

    feat:register user and user admin

--- CW15/task_manager/user/admin.py
+++ CW15/task_manager/user/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+    ]
+    list_filter = ["is_active", "is_staff"]
+    search_fields = ["username", "email"]
+
+
+admin.site.register(User, UserAdmin)
