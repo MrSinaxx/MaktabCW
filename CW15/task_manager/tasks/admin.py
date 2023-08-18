c0e75f09f3b8bafbc36ef8d75b575f0b155dc598
MrSinxx <sinaleylaz@yahoo.com>
2023-08-18 11:11:58 +0330

    feat:created & updated & is_active for task

--- CW15/task_manager/tasks/admin.py
+++ CW15/task_manager/tasks/admin.py
@@ -3,4 +3,17 @@ from .models import Category, Tag, Task
 
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Task)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "status",
+        "created",
+        "updated",
+        "is_active",
+    ]
+    readonly_fields = ["created", "updated", "is_active"]
+
+
+admin.site.register(Task, TaskAdmin)
--- CW15/task_manager/tasks/models.py
+++ CW15/task_manager/tasks/models.py
@@ -35,6 +35,9 @@ class Task(models.Model):
     tags = models.ManyToManyField(Tag)
     files = models.FileField(upload_to="task_files/", blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
